86fcb43d320432f41c6112b5593fa53ebd8cbcbb
dizlord <kazmin1c@gmail.com>
2022-06-18 08:50:06 +0000

    Deploying to gh-pages from @ AntonyUser/fson-55-group-10@1d345e2a16cfb584f3be65d1812908a3ede5ad6e 🚀

---
+++ index.25baf517.js
@@ -0,0 +1,2 @@
+window.onscroll=()=>function(){const n=document.getElementById("header"),s=n.offsetTop;window.pageYOffset>s?n.classList.add("js-no-transparency"):n.classList.remove("js-no-transparency")}();
+//# sourceMappingURL=index.25baf517.js.map
---
+++ index.669d295a.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){return e=document.getElementById("header"),n=e.offsetTop,void(window.pageYOffset>n?e.classList.add("js-no-transparency"):e.classList.remove("js-no-transparency"));var e,n};
+//# sourceMappingURL=index.669d295a.js.map
